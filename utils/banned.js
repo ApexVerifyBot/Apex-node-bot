9f2e47a1588d3613d24444eda03a4fc530c78b8c
ApexVerifyBot <coderxkhalid@gmail.com>
2026-07-12 19:37:05 +0600

    Update banned.js

--- utils/banned.js
+++ utils/banned.js
@@ -1,1 +1,37 @@
+const db = require("./database");
 
+const FILE = "banned_users.json";
+
+async function isBanned(id) {
+
+    const list = await db.read(FILE, []);
+
+    return list.includes(String(id));
+}
+
+async function ban(id) {
+
+    const list = await db.read(FILE, []);
+
+    if (!list.includes(String(id))) {
+
+        list.push(String(id));
+
+        await db.write(FILE, list);
+    }
+}
+
+async function unban(id) {
+
+    let list = await db.read(FILE, []);
+
+    list = list.filter(x => x !== String(id));
+
+    await db.write(FILE, list);
+}
+
+module.exports = {
+    isBanned,
+    ban,
+    unban
+};
