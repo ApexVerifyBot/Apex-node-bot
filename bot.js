3380184ec31b2373532282c903d5f6f95e1b210b
ApexVerifyBot <coderxkhalid@gmail.com>
2026-07-12 19:38:54 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,7 @@
+const users = require("./utils/users");
+const banned = require("./utils/banned");
+const stats = require("./utils/stats");
+const helper = require("./utils/helpers");
 const TelegramBot = require("node-telegram-bot-api");
 
 const config = require("./config");
